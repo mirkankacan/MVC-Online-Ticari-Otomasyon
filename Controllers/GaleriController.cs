e6ab97e7eb8b119c14a0cad03f161949b73ef4c7
Mirkan Kaçan <kacan.m34@gmail.com>
2021-10-08 15:08:14 +0300

    MVC5 Commerical Automation
    Updated 08-10-21 15:07

--- Controllers/GaleriController.cs
+++ Controllers/GaleriController.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using MvcOnlineTicariOtomasyon.Models.Sınıflar;
 namespace MvcOnlineTicariOtomasyon.Controllers
 {
     public class GaleriController : Controller
     {
         // GET: galeri
+        Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+            var degerler = c.Uruns.ToList();
+            return View(degerler);
         }
     }
 }
---
+++ Models/Detay.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models
+{
+    public class Detay
+    {
+        [Key]
+        public int DetayID { get; set; }
+        [Column(TypeName = "Varchar")]
+        [StringLength(30, ErrorMessage = "En fazla 30 karakter girebilirsiniz!")]
+        [Required(ErrorMessage = "Bu alanı boş geçemezsiniz!")]
+        public string UrunAd { get; set; }
+        [Column(TypeName = "Varchar")]
+        [StringLength(3000, ErrorMessage = "En fazla 3000 karakter girebilirsiniz!")]
+        [Required(ErrorMessage = "Bu alanı boş geçemezsiniz!")]
+        public string UrunBilgi { get; set; }
+    }
+}
