2976d6889479ad3b7835ca824f32ea4dc122c0b8
Mirkan Kaçan <kacan.m34@gmail.com>
2021-10-08 17:37:14 +0300

    MVC5 Commercial Automation
    Updated 08-10-21 17:37

--- Controllers/istatistikController.cs
+++ Controllers/istatistikController.cs
@@ -45,7 +45,30 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             ViewBag.d15 = deger15;
             var deger16 = c.SatisHarekets.Where(x => x.Tarih == bugun).Sum(y => y.ToplamTutar).ToString();
             ViewBag.d16 = deger16;
+
             return View();
         }
+
+        public ActionResult KolayTablolar()
+        {
+            var sorgu = from x in c.Carilers
+                        group x by x.CariSehir into g
+                        select new SinifGrup
+                        {
+                            Sehir = g.Key,
+                            Sayi = g.Count()
+                        };
+            return View(sorgu.ToList());
+        }
+        public PartialViewResult Partial1()
+        {
+            var sorgu2 = from x in c.Personels group x by x.Departmanid into g select new SinifGrup2
+            {
+                Departman = g.Key,
+                Sayi = g.Count()
+            };
+            return PartialView(sorgu2.ToList());
+        }
     }
+
 }
